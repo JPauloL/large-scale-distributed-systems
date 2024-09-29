317999e5c3164fb7d30240f37d35cab4dfa95884
JPauloL <jpaulolmail@gmail.com>
2024-09-29 23:28:37 +0100

    Added initial code for lab 1.

---
+++ lab_1/javascript/src/server.js
@@ -0,0 +1,17 @@
+import * as zmq from "zeromq";
+
+async function runServer()
+{
+    const sock = new zmq.Reply();
+    
+    await sock.bind('tcp://*:5555');
+  
+    for await (const [msg] of sock) 
+    {
+        console.log('Received ' + ': [' + msg.toString() + ']');
+        await sock.send('World');
+        // Do some 'work'
+    }
+}
+
+runServer()
